c91521fa4d077f624fc7b385195a8408ed5cdb49
rocity <hello@kvnpl.xyz>
2016-09-13 14:59:27 +0800

    Create store app

--- shoppingcart/shoppingcart/urls.py
+++ shoppingcart/shoppingcart/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'shoppingcart.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^store/', include('store.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
